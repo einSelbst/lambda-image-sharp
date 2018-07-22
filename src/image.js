85d63b5a8c33083cd14b08238d5956bd9502e371
Silvio <silvio@sintuity.com>
2018-07-22 02:05:44 +0200

    add some semicolons to reduce visual noise

--- src/image.js
+++ src/image.js
@@ -1,10 +1,11 @@
-import imageSize from 'image-size'
-import { imgconfig } from './config'
-import { get, upload } from './s3'
-import sharpify from './sharp'
-import { makeKey, decodeS3EventKey } from './utils'
+/* jshint esversion: 6 */
+import imageSize from 'image-size';
+import { imgconfig } from './config';
+import { get, upload } from './s3';
+import sharpify from './sharp';
+import { makeKey, decodeS3EventKey } from './utils';
 
-const { key: fileNameKey, params, output } = imgconfig
+const { key: fileNameKey, params, output } = imgconfig;
 
 let maxWidth = output.default.maxWidth;
 let maxHeight = output.default.maxHeight;
@@ -17,13 +18,13 @@ const imageMimeTypes = {
     webp: 'image/webp',
     gif: 'image/gif',
     svg: 'image/svg+xml',
-}
+};
 
 export default (async function processItem (event) {
-    
+
     const { eventName, s3: { object: { key: undecodedKey } } = { object: { key: false } },} = event;
-    const key = decodeS3EventKey(undecodedKey)
-    
+    const key = decodeS3EventKey(undecodedKey);
+
     const crumbs = key.split('/');
     const directory = crumbs.slice(0, crumbs.length - 1).join('/');
     if (directory != 'undefined' || directory.length > 0) {
@@ -34,56 +35,56 @@ export default (async function processItem (event) {
             outputdir = imageconfig.outputdir ? imageconfig.outputdir : output.default.outputdir;
             transformtype = imageconfig.transformtype ? imageconfig.transformtype : 'sizes';
         }
-    } 
+    }
 
     if (eventName.split(':')[0] !== 'ObjectCreated') {
         throw new Error(
             `Event does not contain a valid type (e.g. ObjectCreated). Invoked by event name: ${eventName}`
-        )
+        );
     }
 
     if (!key) {
-        throw new Error(`Event does not contain a valid S3 Object Key. Invoked with key: ${key}`)
+        throw new Error(`Event does not contain a valid S3 Object Key. Invoked with key: ${key}`);
     }
 
-    const { Body: image, ContentType: type } = await get({ Key: key })
+    const { Body: image, ContentType: type } = await get({ Key: key });
 
-    const dimensions = imageSize(image)
+    const dimensions = imageSize(image);
 
     // convert it into a string.
-    const width = parseInt(maxWidth, 10)
+    const width = parseInt(maxWidth, 10);
 
-    const aspectRatio = dimensions.width / dimensions.height
-    const height = []
+    const aspectRatio = dimensions.width / dimensions.height;
+    const height = [];
 
     // Width has to be an integer for sharp.
-    const sizes = []
+    const sizes = [];
 
     if (transformtype === "responsive") {
-        sizes.push(width)
-        sizes.push(Math.round(width * 1.5))
-        sizes.push(width * 2)
-        sizes.push(width * 3)
+        sizes.push(width);
+        sizes.push(Math.round(width * 1.5));
+        sizes.push(width * 2);
+        sizes.push(width * 3);
     } else {
-        sizes.push(Math.round(width / 4))
-        sizes.push(Math.round(width / 2))
-        sizes.push(width)
-        sizes.push(Math.round(width * 1.5))
-        sizes.push(width * 2)
-        sizes.push(width * 3)
+        sizes.push(Math.round(width / 4));
+        sizes.push(Math.round(width / 2));
+        sizes.push(width);
+        sizes.push(Math.round(width * 1.5));
+        sizes.push(width * 2);
+        sizes.push(width * 3);
     }
 
     sizes.map((wth) => {
         if (maxHeight === 'undefined' || maxHeight === null) {
-            height.push(Math.round(wth / aspectRatio))
+            height.push(Math.round(wth / aspectRatio));
         } else {
-            height.push(Math.round(wth * (maxHeight / maxWidth)))
+            height.push(Math.round(wth * (maxHeight / maxWidth)));
         }
-        return wth
-    })
+        return wth;
+    });
 
-    const streams = await sharpify(image, imgconfig, sizes, 610)
-    const context = { key, type }
+    const streams = await sharpify(image, imgconfig, sizes, 610);
+    const context = { key, type };
 
     return Promise.all(
         streams.map(async (stream, index) => {
@@ -91,7 +92,7 @@ export default (async function processItem (event) {
                 ContentType: imageMimeTypes[(await stream.metadata()).format],
                 ...params,
                 Key: makeKey(fileNameKey, context, sizes[index], outputdir),
-            })
+            });
         })
-    )
-})
+    );
+});
